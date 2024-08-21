2651d47575a1def24b1ad20b5a4769960550635b
Chenxiong Qi <qcxhome@gmail.com>
2024-08-21 21:01:53 +0800

    check non-existing PipelineRun file

--- migration-tool.sh
+++ migration-tool.sh
@@ -3,10 +3,13 @@
 echo "THIS IS FOR CHECKING FROM WHERE RENOVATE FINDS OUT THE EXECUTABLE FOR A POST-UPGRADE TASK."
 
 plr_file=$1
-[ -f "$plr_file" ] || exit
+if [ ! -f "$plr_file" ]; then
+    echo "PipelineRun file $plr_file does not exist." >&2
+    exit 1
+fi
 
 image_ref=$(yq '.spec.tasks[] | select(.name == "init") | .taskRef.params[] | select(.name == "bundle") | .value' "$plr_file")
 digest=${image_ref#*@}
 image_without_digest=${image_ref%@*}
 image_repo=${image_without_digest%:*}
-skopeo inspect "ocker://${image_repo}@${digest}"
+skopeo inspect "docker://${image_repo}@${digest}"
