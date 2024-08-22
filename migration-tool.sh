6511143a26b2db3e264a75d674fec19a2ce251f7
Chenxiong Qi <qcxhome@gmail.com>
2024-08-22 20:51:53 +0800

    update migration tool to inspect pipeline bundle

--- migration-tool.sh
+++ migration-tool.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
 plr_file=$1
 if [ ! -f "$plr_file" ]; then
     echo "PipelineRun file $plr_file does not exist." >&2
@@ -23,3 +25,21 @@ image_repo=${image_without_digest%:*}
 echo "inspect image: $to_image_ref"
 echo "inspect image: $to_image_ref" >>README.md
 skopeo inspect --no-tags "docker://${image_repo}@${digest}" >>README.md
+
+expected_image_repo=quay.io/konflux-ci/tekton-catalog/task-summary
+
+if [ "$image_repo" != "$expected_image_repo" ]; then
+    echo "Skip handling image from $image_repo"
+    exit 0
+fi
+
+tag=$(
+    curl -s "https://quay.io/api/v1/repository/${image_repo}/tag/" | \
+    jq -r ".tags[] | select(.manifest_digest == \"${digest}\") | select(.name | test(\"^[0-9.]+-[0-9a-f]+$\")) | .name"
+)
+revision=${tag#*-}
+
+pl_bundle_ref="quay.io/konflux-ci/tekton-catalog/pipeline-docker-build:${revision}"
+echo
+echo "inspect pipeline bundle: $pl_bundle_ref"
+skopeo inspect --no-tags "docker://${pl_bundle_ref}"
