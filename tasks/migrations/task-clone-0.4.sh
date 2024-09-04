4d1c2b326a3e31bd2fff59e9cf624f17f287c379
Chenxiong Qi <qcxhome@gmail.com>
2024-09-04 22:01:19 +0800

    migrate: check existence firstly

--- tasks/migrations/task-clone-0.4.sh
+++ tasks/migrations/task-clone-0.4.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 pipeline=$1
 task_name=clone
+param=$(yq ".spec.tasks[] | select(.name == \"${task_name}\") | .params[] | select(.name == \"sslVerify\" and .value == \"false\")" "$pipeline")
+[ -z "$param" ] && \
 yq -i "(.spec.tasks[] | select(.name == \"${task_name}\") | .params) += {\"name\": \"sslVerify\", \"value\": \"false\"}" "$pipeline"
