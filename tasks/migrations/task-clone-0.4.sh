2479f9ee678d905ccd6143b082f482757c9d63d4
Chenxiong Qi <qcxhome@gmail.com>
2024-09-04 23:29:00 +0800

    migrate: fix

--- tasks/migrations/task-clone-0.4.sh
+++ tasks/migrations/task-clone-0.4.sh
@@ -2,5 +2,7 @@
 pipeline=$1
 task_name=clone
 param=$(yq ".spec.tasks[] | select(.name == \"${task_name}\") | .params[] | select(.name == \"sslVerify\" and .value == \"false\")" "$pipeline")
-[ -z "$param" ] && \
-yq -i "(.spec.tasks[] | select(.name == \"${task_name}\") | .params) += {\"name\": \"sslVerify\", \"value\": \"false\"}" "$pipeline"
+if [ -z "$param" ]
+then
+  yq -i "(.spec.tasks[] | select(.name == \"${task_name}\") | .params) += {\"name\": \"sslVerify\", \"value\": \"false\"}" "$pipeline"
+fi
