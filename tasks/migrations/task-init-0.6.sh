1735fc25464f0721b80902e8f20bdb8e004bc10e
Chenxiong Qi <qcxhome@gmail.com>
2024-09-05 00:27:17 +0800

    migrate(fictional): add task coverage to local plr

---
+++ tasks/migrations/task-init-0.6.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+pipeline=$1
+coverage_task_exists=$(
+yq '
+.spec.pipelineSpec.tasks[] | select(.name == "coverage") |
+.taskRef.params[] | select(.name == "bundle") |
+.value | contains("quay.io/mytestworkload/test-renovate-updates-task-coverage")
+' "$pipeline"
+)
+
+if [ -z "$coverage_task_exists" ]
+then
+  task_def='{
+  "name": "coverage",
+  "taskRef": {
+    "params": [
+      {"name": "name", "value": "coverage"},
+      {"name": "bundle", "value": "quay.io/mytestworkload/test-renovate-updates-task-coverage@sha256:f1035491051492d27ba637bfe626fee532a1e9c2b547cae79fc7504a62fbf763"},
+      {"name": "kind", "value": "task"}
+    ]
+  },
+  "runAfter": ["test"]
+}'
+  yq -i ".spec.pipelineSpec.tasks += ${task_def}" "$pipeline"
+fi
