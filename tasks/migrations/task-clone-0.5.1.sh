43fa61b9f4e88d428d0dceffb1e2d0eace74d84b
Chenxiong Qi <qcxhome@gmail.com>
2024-09-05 18:02:04 +0800

    migrate: add workspace to pipeline
    Migration is added to task clone. The migration will add the workspace
    to all necessary tasks.

---
+++ tasks/migrations/task-clone-0.5.1.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+pipeline=$1
+
+# update tasks: clone, test, coverage
+
+for task in clone "test" coverage; do
+  expr="
+  .spec.tasks[] | select(.name == \"${task}\") |
+  .workspaces[] | select(.name == \"source\" and .workspace == \"workspace\")
+  "
+  ws=$(yq "$expr" "$pipeline")
+
+  if [ -z "$ws" ]
+  then
+    new_ws='[{"name": "source", "workspace": "workspace"}]'
+    expr="(.spec.pipelineSpec.tasks[] | select(.name == \"${task}\") | .workspaces) += ${new_ws}"
+    yq -i "$expr" "$pipeline"
+  fi
+done
