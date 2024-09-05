bbc7636b55c0f65c0bc99ae70b409002db40d5de
Chenxiong Qi <qcxhome@gmail.com>
2024-09-05 10:27:00 +0800

    fix(migrate): wrong tasks path

--- tasks/migrations/task-init-0.7.sh
+++ tasks/migrations/task-init-0.7.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 pipeline=$1
 
-if ! yq '.spec.pipelineSpec.tasks[] | select(.name == "test") | .runAfter' "$pipeline" | grep -q "\- clone" >/dev/null
+if ! yq '.spec.tasks[] | select(.name == "test") | .runAfter' "$pipeline" | grep -q "\- clone" >/dev/null
 then
-  yq -i '(.spec.pipelineSpec.tasks[] | select(.name == "test")) += {"runAfter": ["clone"]}' "$pipeline"
+  yq -i '(.spec.tasks[] | select(.name == "test")) += {"runAfter": ["clone"]}' "$pipeline"
 fi
