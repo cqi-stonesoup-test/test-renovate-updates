83db42f248483c5e31a222ca4b350703b4eb7490
Chenxiong Qi <qcxhome@gmail.com>
2024-09-10 10:28:46 +0800

    fix(migrate): wrong bash script

--- tasks/migrations/task-coverage-0.1.2.sh
+++ tasks/migrations/task-coverage-0.1.2.sh
@@ -6,7 +6,7 @@ task_name=coverage
 remove_item_from_array() {
     local -r remove_item=$1
     local -r target_file=$2
-    task_names=$(yq f'.spec | .tasks[] | select(.name == "coverage") | .runAfter[]' "$target_file" | nl -v 0)
+    task_names=$(yq '.spec | .tasks[] | select(.name == "coverage") | .runAfter[]' "$target_file" | nl -v 0)
     grep "$remove_item" <<<"$task_names" | while read -r idx task_name; do
         yq -i "del(.spec | .tasks[] | select(.name == \"coverage\") | .runAfter[$idx])" "$target_file"
     done
