95c412fc032c26323bfd1d5ef4332a6cc8fe72f1
Chenxiong Qi <qcxhome@gmail.com>
2024-09-10 09:06:31 +0800

    migrate: demo of removing items from an array field

---
+++ tasks/migrations/task-coverage-0.1.2.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+pipeline=$1
+task_name=coverage
+
+remove_item_from_array() {
+    local -r remove_item=$1
+    local -r target_file=$2
+    task_names=$(yq f'.spec | .tasks[] | select(.name == "coverage") | .runAfter[]' "$target_file" | nl -v 0)
+    grep "$remove_item" <<<"$task_names" | while read -r idx task_name; do
+        yq -i "del(.spec | .tasks[] | select(.name == "coverage") | .runAfter[$idx])" "$target_file"
+    done
+}
+remove_item_from_array clone "$pipeline"
+remove_item_from_array init "$pipeline"
+yq -i '(.spec | .tasks[] | select(.name == "coverage") | .runAfter) += ["git-clone", "initialize"]' "$pipeline"
+yq -i '(.spec | .tasks[] | select(.name == "coverage") | .runAfter) |= unique' "$pipeline"
